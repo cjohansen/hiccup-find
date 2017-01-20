cd987a59934596a06935d9d9e6987a579b7b5525
Ian Truslove <ian.truslove@gmail.com>
2017-01-19 19:14:03 -0700

    Update to current deps
    The clojure dependency will be required for cljc

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://github.com/cjohansen/hiccup-find"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6.0"]]
-                   :plugins [[lein-midje "3.1.3"]]}})
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2.1"]]}})
