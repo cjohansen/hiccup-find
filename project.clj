3cab6a2cf1bd07df7b695c87b1403d42596e124c
Ian Truslove <ian.truslove@gmail.com>
2017-01-19 19:10:57 -0700

    :dev profile is a better place to run `lein midje`

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:test {:dependencies [[midje "1.6.0"]]
-                    :plugins [[lein-midje "3.1.3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6.0"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
