e1d0105156141d4ecd33fc651a7ba929a00aba6a
Ian Truslove <ian.truslove@gmail.com>
2017-01-19 19:25:20 -0700

    Remove midje deps

--- project.clj
+++ project.clj
@@ -3,6 +3,4 @@
   :url "http://github.com/cjohansen/hiccup-find"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[midje "1.8.3"]]
-                   :plugins [[lein-midje "3.2.1"]]}})
+  :dependencies [[org.clojure/clojure "1.8.0"]])
